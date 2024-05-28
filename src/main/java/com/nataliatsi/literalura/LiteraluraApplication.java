915d6bdc6b6bab783a44618effc840910421b690
Natália S. Gomes <nataliandsg32@gmail.com>
2024-05-28 16:29:41 -0300

    Initial commit

---
+++ src/main/java/com/nataliatsi/literalura/LiteraluraApplication.java
@@ -0,0 +1,20 @@
+package com.nataliatsi.literalura;
+
+import com.nataliatsi.literalura.principal.Menu;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Menu menu = new Menu();
+        menu.exibeMenu();
+    }
+}
